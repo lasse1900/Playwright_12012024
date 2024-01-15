f66e07158bbf15f4da15283e8f2f33ce2a1c4a8c
lasse1900 <lauri.kyttala@gmail.com>
2024-01-15 15:21:10 +0200

    Section 2 examples added

---
+++ playwright.config_original_short.js
@@ -0,0 +1,25 @@
+// @ts-check
+// according to original
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
+
